0f83ecf3698780b73592eaa9eb8ba4c46e42aec2
D4rkr34lm <darth.m.frohn@gmail.com>
2025-02-14 23:49:40 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("config.lazy");
+
+vim.cmd("colorscheme tokyonight");
+
+vim.opt.background = "dark";
+
+vim.opt.number = true;
+vim.opt.cursorline = true;
+
+vim.opt.confirm = true;
+
+vim.opt.autoindent = true;
+vim.opt.smartindent = true;
+
+vim.opt.spell = true;
+vim.opt.spelloptions = {"camel"};
+
+vim.opt.tabstop = 2;
+vim.opt.shiftwidth = 2;
