f393c2474f6beee53258e44b7ea9a29ede81c092
Manuel Frohn <manuel.frohn.09.12@gmail.com>
2025-02-25 18:02:38 +0100

    Added responsivness to formating, configured snacks further

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,6 +1,15 @@
+local fidget = require("fidget")
+
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
+		fidget.notify("Formating ...", vim.log.levels.INFO)
+		require("conform").format({ bufnr = args.buf }, function(err)
+			if err then
+				vim.notify("Formating failed: " .. err, vim.log.levels.ERROR)
+			else
+				fidget.notify("Formating completed", vim.log.levels.INFO)
+			end
+		end)
 	end,
 })
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,14 +7,17 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
-		-- bigfile = { enabled = true },
+		util = { enabled = true },
+		bigfile = { enabled = true },
+		bufdelete = { enabled = true },
 		-- dashboard = { enabled = true },
 		explorer = { enabled = true },
-		-- indent = { enabled = true },
+		indent = { enabled = true },
 		-- input = { enabled = true },
 		-- picker = { enabled = true },
-		-- notifier = { enabled = true },
-		-- quickfile = { enabled = true },
+		notfy = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
 		-- scope = { enabled = true },
 		-- scroll = { enabled = true },
 		-- statuscolumn = { enabled = true },
